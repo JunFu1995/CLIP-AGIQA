803bc40a21fc7005b1b4749c20cf54147a00df15
fujun <1332536533@qq.com>
2024-07-13 16:38:43 +0800

    initial

--- autoRun.py
+++ autoRun.py
@@ -7,8 +7,8 @@ pynvml.nvmlInit()
 
 def taskAux():
     cmd_list = []
-    for m in ['AGIQA_clean']:
-        for ds in ['AGIQA3k']: #,'AGIQA2023'
+    for m in ['AGIQA']:
+        for ds in ['AGIQA3k']: #, 'AGIQA2023']: 
             cmd = 'nohup python train_test_clip_auxiliary.py --dataset %s --model %s &' % (ds, m)
             cmd_list.append(cmd)
     return cmd_list
@@ -18,7 +18,8 @@ cmd = taskAux()
 while cmd:
     handle = pynvml.nvmlDeviceGetHandleByIndex(0)
     memoinfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-    if memoinfo.used / 1e6 < 14 * 1000:
+    #print(memoinfo.used)
+    if memoinfo.used / 1e6 < 14 * 1000: # MB
         c = cmd.pop(0)
         subprocess.call(c, shell=True)
         time.sleep(15)
